01f2b4826d4a8eb705944eed14ba8081d4e53d75
PedroM <112040341+OhruzZ@users.noreply.github.com>
2022-08-31 20:39:00 -0300

    Update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,22 @@
-/**
- * 
- */
-/**
- * @author ALUNO
- *
- */
-module AtividadeRodr1 {
-}
+import java.util.Scanner;
+
+public class atvd1 {
+	 public static void main(String[] args) {
+	        int nota1, nota2, media; 
+	        Scanner input = new Scanner(System.in);
+	        System.out.println("Digite a nota do primeiro bimestre:");
+	        nota1 = input.nextInt();
+	        nota1= nota1*2;
+	        System.out.println("Digite a nota do segundo bimestre:");
+	        nota2 = input.nextInt();
+	        nota2= nota2*3;
+	        
+	        media = ((nota1 + nota2)/2);
+	        System.out.println("A media final e : " + media);
+	        
+	        
+	        
+	    }
+	}
+
+
